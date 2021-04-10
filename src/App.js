3f2c9e0bd7b253a7ecb89787aa969c667e2d2a03
Maamun <mamun.mokni@gmail.com>
2021-04-11 01:36:48 +0400

    Styling the component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>HI</h1>
+      <div className="wrapper">
+        <h1>My Counter</h1>
+        <p>37</p>
+        <div className="btn-wrapper">
+          <button className="btn">
+            -
+          </button>
+          <input
+            className="input"
+            type="text"
+            value="6"
+            placeholder="increment/decrement by"
+          />
+          <button className="btn">
+            +
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
