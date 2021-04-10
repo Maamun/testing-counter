284c2e5ebd3a4311c2da2aa7625916578242f6d4
Maamun <mamun.mokni@gmail.com>
2021-04-11 03:03:47 +0400

    Writing First Test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import logo from './logo.svg';
-import './App.scss';
+import Counter from './Counter/Counter';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="wrapper">
-        <h1>My Counter</h1>
-        <p>37</p>
-        <div className="btn-wrapper">
-          <button className="btn">
-            -
-          </button>
-          <input
-            className="input"
-            type="text"
-            value="6"
-            placeholder="increment/decrement by"
-          />
-          <button className="btn">
-            +
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return <Counter />;
 }
 
 export default App;
